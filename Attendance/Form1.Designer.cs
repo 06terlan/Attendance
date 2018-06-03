8dccd77f968d4c0230afc40b194eff81f59560fb
06terlan <06terlan91@gmail.com>
2018-06-03 23:55:09 +0400

    init

---
+++ Attendance/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Attendance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_file = new System.Windows.Forms.Button();
+            this.btn_report = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_file
+            // 
+            this.btn_file.Location = new System.Drawing.Point(12, 12);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(174, 48);
+            this.btn_file.TabIndex = 0;
+            this.btn_file.Text = "Choose file";
+            this.btn_file.UseVisualStyleBackColor = true;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // btn_report
+            // 
+            this.btn_report.Location = new System.Drawing.Point(192, 12);
+            this.btn_report.Name = "btn_report";
+            this.btn_report.Size = new System.Drawing.Size(174, 48);
+            this.btn_report.TabIndex = 1;
+            this.btn_report.Text = "Report";
+            this.btn_report.UseVisualStyleBackColor = true;
+            this.btn_report.Click += new System.EventHandler(this.btn_report_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(524, 70);
+            this.Controls.Add(this.btn_report);
+            this.Controls.Add(this.btn_file);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Report";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_file;
+        private System.Windows.Forms.Button btn_report;
+    }
+}
+
---
+++ Attendance/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Data.OleDb;
+
+namespace Attendance
+{
+    public partial class Form1 : Form
+    {
+        IDictionary<int, DateTime[]> dict = new Dictionary<int, DateTime[]>();
+        IDictionary<int, bool> dictChk = new Dictionary<int, bool>();
+        const string fileName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void btn_report_Click(object sender, EventArgs e)
+        {
+            getUsersFromExcel();
+            CreateReport();
+            writeDataToExcel();
+        }
+
+        private void getUsersFromExcel()
+        {
+            var fileName = string.Format("{0}\\SWH.xls", Directory.GetCurrentDirectory());
+            var connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", fileName);
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand("select ID from [Sheet1$]", connection);
+                using (OleDbDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        string str = dr[0].ToString().Trim();
+                        if (str != "")
+                        {
+                            int row1Col0 = int.Parse(str);
+                            dict[row1Col0] = new DateTime[2];
+                            dictChk[row1Col0] = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CreateReport()
+        {
+            string line;
+            var fileName = string.Format("{0}\\History.txt", Directory.GetCurrentDirectory());
+
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] datas = line.Split(';');
+                if (datas.Length >= 7)
+                {
+                    int sId = 0;
+                    DateTime time = new DateTime();
+                    string sTime = datas[0].Trim('\"');
+                    string tur = datas[5].Trim('\"');
+
+                    if (int.TryParse(datas[7].Trim('\"'), out sId) && sTime != "")
+                    {
+                        if (dict.ContainsKey(sId))
+                        {
+                            time = DateTime.Parse(sTime);
+                            //in
+                            if (tur == "RS-485/PCI-Panel1-R2" || tur == "RS-485/PCI-Panel1-R4" || tur == "RS-485/PCI-Panel4-HR-in")
+                            {
+                                dict[sId][0] = DateTime.Compare(dict[sId][0], new DateTime()) == 0 || DateTime.Compare(dict[sId][0], time) > 0 ? time : dict[sId][0];
+                            }
+                            else if (tur == "RS-485/PCI-Panel1-R1" || tur == "RS-485/PCI-Panel1-R3" || tur == "RS-485/PCI-Panel4-HR-out")
+                            {
+                                dict[sId][1] = DateTime.Compare(dict[sId][1], new DateTime()) == 0 || DateTime.Compare(dict[sId][1], time) < 0 ? time : dict[sId][1];
+                            }
+                            dictChk[sId] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void writeDataToExcel()
+        {
+            var fileName = string.Format("{0}\\SWH.xls", Directory.GetCurrentDirectory());
+
+            Excel.Workbook MyBook = null;
+            Excel.Application MyApp = null;
+            Excel.Worksheet MySheet = null;
+
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            MyBook = MyApp.Workbooks.Open(fileName);
+            MySheet = MyBook.Sheets[1];
+            Excel.Range usedRange = MySheet.UsedRange;
+            int countRows = usedRange.Rows.Count;
+
+
+            MyApp.Workbooks.Close();
+        }
+    }
+}
